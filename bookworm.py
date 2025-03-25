6a5d25683c80826ed13f856e86f18d768e92baf4
kevan <garmal30.kwjw@gmail.com>
2025-03-25 21:32:17 +0800

    Update bookworm.py

--- bookworm.py
+++ bookworm.py
@@ -1,11 +1,15 @@
 import os
-from scrapers import amazon, thryft
+import isbnformat
+from scrapers import amazon, kinokuniya, thryft
 
 timeout = 5
+threads = 25 # webscraping is i/o based so depends on user's ram generally can try anywhere between 20-50
+
+# note: for threads, thryft seems to have a rate limit, 5 tentatively seems to work
 
 #inputs for user
 while True:
-    inputname = input("Enter the input CSV file name (including .csv): ")
+    inputname = input("Enter the input CSV/XLSX file name (including .csv or .xlsx): ")
     if os.path.isfile(inputname):
         break
     else:
@@ -18,10 +22,17 @@ while True:
     else:
         print("Output file name must end with '.csv'. Please try again.")
 
-print("\nstarting amazon price scraping... (⊙ Д⊙ )")
-amazon.amazon(inputname, outputname, timeout)
+print("/n formatting isbn13...")
+isbnformat.isbnformat(inputname, outputname)
 
-print("\nstarting thryft price scraping... |ω • ´)")
-thryft.thryft(outputname, outputname, timeout)
+print("\nstarting amazon price scraping...")
+amazon.amazon(outputname, outputname, timeout, threads)
 
-print("\nscraping completed! (´・ω・｀) \nresults saved to:", outputname)
+print("\nstarting kinokuniya price scraping...")
+kinokuniya.kinokuniya(outputname, outputname, timeout, threads)
+
+# thryft rate limits to a max of 5 threads...
+print("\nstarting thryft price scraping...")
+thryft.thryft(outputname, outputname, timeout, 5)
+
+print("\nscraping completed! \nresults saved to:", outputname)
